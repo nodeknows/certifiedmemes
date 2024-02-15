70ed2b1d5c711be144dbea65e8e49741bac6a86c
Asian Himothy <evan.yang23@student.cart.org>
2024-02-15 09:52:50 -0800

    .

--- script.js
+++ script.js
@@ -1,45 +1,49 @@
-var form = document.getElementsByTagName('form')[0]
-var firstname = document.getElementById('firstname'), lastname = document.getElementById('lastname');
-var lang = document.getElementById('lang');
-var len = document.getElementById('len');
+const getElem = (id) => document.getElementById(id)
+const clearCanvas = (ctx, canv) => ctx.clearRect(0, 0, canv.width, canv.height);
+const getDate = () => new Date().toLocaleDateString('en-us', {day:'numeric', year:'numeric', month:'numeric'});
 
-var canvas = document.getElementById('canvas')
+function getDist(l, fn, ln) {
+    if (l.value ===  'Auto') {
+        return (fn.value.length + ln.value.length > 11) ? 'far' : 'close';
+    };
+    return (l.value === 'Close') ? 'close' : 'far';
+};
+
+var form = document.getElementsByTagName('form')[0]
+var firstname = getElem('firstname'), lastname = getElem('lastname');
+var lang = getElem('lang');
+var len = getElem('len');
+
+var canvas = getElem('canvas')
 var ctx = canvas.getContext('2d');
 
 
 // stop page from refreshing
 form.addEventListener('submit', function (e) { e.preventDefault() })
 
-form.onsubmit = function (e) {
-    let t;
+form.onsubmit = function (e) {    
+    clearCanvas(ctx, canvas); // clears canvas everytime submit is pressed; not necessary however.
 
-    if (len.value ===  'Auto') {
-        if ((firstname.value.length + lastname.value.length) > 11) { t = 'far' } else { t = 'close' };
-    } else if (len.value === 'Close') {
-        t = 'close';
-    } else if (len.value === 'Far') {
-        t = 'far';
-    }
-    
-    ctx.clearRect(0, 0, canvas.width, canvas.height); // clears canvas everytime submit is pressed; but not necessary
+    let dist = getDist(len, firstname, lastname); // t = close or far; determines the claw on the certificate's gap.
+    let color = (lang.value === 'JS') ? "black" : "white"; // var myVariable = (true condition) ? "true" : "false"
 
-    img = document.createElement('img');
-    img.src = `/templates/${lang.value}_${t}.webp`;
+    // create template & draw img
+    const img = document.createElement('img');
+    img.src = `/templates/${lang.value}_${dist}.webp`;
     img.style.width = "100%";
     img.style.height = "100%";
 
     ctx.drawImage(img, 0, 0, 3508, 2479);
 
-    if (lang.value === 'JS') ctx.fillStyle = 'black';
-    if (lang.value !== 'JS') ctx.fillStyle = 'white';
+    // prep & write text
+    ctx.fillStyle = color;
 
     ctx.font = '9rem inknut';
     ctx.textAlign = 'center';
     ctx.fillText(`${firstname.value} ${lastname.value}`, canvas.width / 2, canvas.height / 2 + 185);
 
-    let theDate = new Date().toLocaleDateString('en-us', {day:'numeric', year:'numeric', month:'numeric'})
     ctx.font = '7rem barcode';
-    ctx.fillText(theDate, canvas.width / 2 - 325, canvas.height - 190);
+    ctx.fillText(getDate(), canvas.width / 2 - 325, canvas.height - 190);
 
 }
 
@@ -47,5 +51,5 @@ function printCanvas() {
     let url = canvas.toDataURL();
     let win = window.open();
     win.document.write("<img src='" + url + "' style=\"width: 100%; height: 100%\"/>");
-    setTimeout(() => win.print(), 0); // write first, then print
+    setTimeout(() => win.print(), 0);
 }
